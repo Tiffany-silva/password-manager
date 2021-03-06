9656d4a41b2bfc48db2f977699663a4ce9cba7a0
Tiffany <supeshalapragathisilva@gmail.com>
2021-03-07 00:30:14 +0530

    Login UI component added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {RegistrationComponent} from './UI-pages/registration/registration.component';
+import {LoginComponent} from './UI-pages/login/login.component';
 
 const routes: Routes = [
   {path: '', component: RegistrationComponent},
-  {path: 'register', component: RegistrationComponent}
+  {path: 'register', component: RegistrationComponent},
+  {path: 'login', component: LoginComponent}
 ];
 
 @NgModule({
