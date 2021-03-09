95dbf9b99bffe238457c790b404d3c9681830e05
Tiffany <supeshalapragathisilva@gmail.com>
2021-03-09 21:23:59 +0530

    added category service

---
+++ src/app/Services/category/category.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+const baseUrl = 'http://localhost:8080/api/category';
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+
+  constructor(private http: HttpClient) { }
+
+  getAllCategory(): Observable<any> {
+    return this.http.get(baseUrl+"/findAllCategories", { responseType: 'json' });
+  }
+
+  createCategory(data:any): Observable<any> {
+    return this.http.post(baseUrl,data);
+  }
+
+}
