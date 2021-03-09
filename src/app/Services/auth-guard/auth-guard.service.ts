0707a232bb0a50f967bc140d24a9d56e17f82712
Tiffany <supeshalapragathisilva@gmail.com>
2021-03-09 21:24:34 +0530

    added auth service

---
+++ src/app/Services/auth-guard/auth-guard.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router} from '@angular/router';
+import {TokenStorageService} from '../token-storage/token-storage.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuardService implements CanActivate{
+
+	constructor(public token: TokenStorageService, public router: Router) {}
+
+	canActivate(): boolean {
+	  if (!this.token.isAuthenticated()) {
+	    this.router.navigate(['login']);
+	    return false;
+	  }
+	  return true;
+	}
+
+}
